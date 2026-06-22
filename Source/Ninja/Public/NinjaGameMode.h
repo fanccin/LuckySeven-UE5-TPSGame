b36c596d1dafd469adc900209e936e99a32b335e
wh1150 <80726310+wh1150@users.noreply.github.com>
2026-06-22 10:48:46 +0900

    클래스 생성

---
+++ Source/Ninja/Public/NinjaGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "NinjaGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NINJA_API ANinjaGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Ninja/Public/NinjaGameState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "NinjaGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NINJA_API ANinjaGameState : public AGameState
+{
+	GENERATED_BODY()
+	
+};
