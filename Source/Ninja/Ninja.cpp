83458d2d936829876428661fc19a8c7c80dd818b
fanccin <77671419+fanccin@users.noreply.github.com>
2026-06-22 10:22:14 +0900

    Initial Commit

---
+++ Source/Ninja/Ninja.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Ninja.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Ninja, "Ninja" );
