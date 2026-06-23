9119ce295f0e5d3fbf646ba6c6ef9be3dfb10fa0
variable <jegalvariable@gmail.com>
2026-06-23 12:09:30 +0900

    #6 TurnActor 완성

--- Source/Ninja/Public/TurnActor.h
+++ Source/Ninja/Public/TurnActor.h
@@ -4,8 +4,19 @@
 #include "DefaultActor.h"
 #include "TurnActor.generated.h"
 
+class UTurnActorComponent;
+
 UCLASS()
 class NINJA_API ATurnActor : public ADefaultActor
 {
 	GENERATED_BODY()
+	
+public:
+	ATurnActor();
+	
+	virtual void Tick(float DeltaTime) override;
+	
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Turn|Components")
+	UTurnActorComponent* TurnComp;
 };
