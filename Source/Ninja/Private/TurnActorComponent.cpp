44cd87acbd40ed19e5e77652478c8ff5eeaf636a
variable <jegalvariable@gmail.com>
2026-06-23 10:21:12 +0900

    #6 TurnComponent로 변경

---
+++ Source/Ninja/Private/TurnActorComponent.cpp
@@ -0,0 +1,23 @@
+#include "TurnActorComponent.h"
+
+UTurnActorComponent::UTurnActorComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UTurnActorComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Owner = GetOwner(); // 컴포넌트를 소유하고 있는 엑터를 Owner에 저장
+}
+
+void UTurnActorComponent::Turn(float DeltaTime)
+{
+	if (!Owner) // 안전 검사
+	{
+		return;
+	}
+	
+	Owner->AddActorLocalRotation(RotationInput * RotationSpeed * DeltaTime);
+}
