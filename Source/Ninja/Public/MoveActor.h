e4048884685131ca87eca0fb628f95753c349028
variable <jegalvariable@gmail.com>
2026-06-23 10:52:23 +0900

    #6 MoveActor, TurnActor 생성

---
+++ Source/Ninja/Public/MoveActor.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DefaultActor.h"
+#include "MoveActor.generated.h"
+
+UCLASS()
+class NINJA_API AMoveActor : public ADefaultActor
+{
+	GENERATED_BODY()
+};
---
+++ Source/Ninja/Public/TurnActor.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DefaultActor.h"
+#include "TurnActor.generated.h"
+
+UCLASS()
+class NINJA_API ATurnActor : public ADefaultActor
+{
+	GENERATED_BODY()
+};
