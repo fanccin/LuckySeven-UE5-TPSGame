cbef812ffaaaf2d183c909b7182ef33e9f16966f
DevCol <devcol.main@gmail.com>
2026-06-25 19:17:26 +0900

    #45 Delete NinjaCharacterController cpp & h
    #45 Delete NinjaCharacterController cpp & h

--- Source/Ninja/Private/NinjaCharacter.cpp
+++ Source/Ninja/Private/NinjaCharacter.cpp
@@ -5,7 +5,7 @@
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h" 
 #include "GameFramework/CharacterMovementComponent.h" 
-#include "NinjaCharacterController.h"
+
 #include "NinjaCharacterStatData.h"
 #include "NinjaGameState.h"
 
